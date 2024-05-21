72a3cd809010fbf969af338fb29844c3fadc56b4
Oviyasenthilkumar <oviyasenthilkumar03@gmail.com>
2024-05-21 10:27:58 +0530

    react sample project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/Login";
+
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddEmployee from "./components/AddEmployee";
+import EmployeeList from "./components/EmployeeList";
+import UpdateTask from "./components/UpdateTask";
+
+
 
 function App() {
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Login/>}/>
+      <Route path="dashboard" element={<Dashboard/>}/>
+      <Route path="addEmployee" element={<AddEmployee/>}/>
+      <Route path="employeeList" element={<EmployeeList/>} />
+      <Route path="updateTask" element={<UpdateTask/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
